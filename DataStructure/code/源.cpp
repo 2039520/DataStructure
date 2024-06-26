fb43c503faf72fe057b47e5525012cff9e2bd852
2039520 <58757138+2039520@users.noreply.github.com>
2024-06-26 13:37:11 +0800

    Add files via upload

---
+++ DataStructure/code/源.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NO_VA_START_VALIDATION
+#include<iostream>
+#include<string>
+#include"LinkList.h"
+#include"SqStack.h"
+#include"Array.h"
+#include"GList.h"
+#include"String.h"
+#include"LinkBiTree.h"
+#include"ThrBiTree.h"
+#include"DynamicSearchTable.h"
+#include"StraightInsertionSort.h"
+#include"ShellSort.h"
+#include"QSort.h"
+#include"SelectSort.h"
+#include"HeapSort.h"
+#include"MergeSort.h"
+#include<cstdio>
+
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	scanf("%d,%d", &a, &b);
+	printf("%d//%d",a, b);
+	system("pause");
+	return 0;
+}
